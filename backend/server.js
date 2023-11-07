76f315d3900aa6e4c4c53fce6b06025e118083c9
Michelle <mvflandin@gmail.com>
2023-11-07 13:38:59 -0800

    Add nodemailer setup

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3001;
@@ -12,3 +14,38 @@ app.get('/api', (req, res) => {
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
+
+// NODEMAILER SETUP
+
+const nodemailer = require('nodemailer');
+
+// Create a transporter object using the default SMTP transport
+let transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    secure: false,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS
+    }
+  });
+
+// Email sending route
+app.post('/api/sendmail', async (req, res) => {
+    let { name, email, subject, message } = req.body;
+  
+    try {
+      // send mail with defined transport object
+      let info = await transporter.sendMail({
+        from: '"My Website Contact Form" <your-email@example.com>', // sender address
+        to: "your-receiving-email@example.com", // list of receivers
+        subject: subject, // Subject line
+        text: `Message from: ${name} <${email}>\n\n${message}`, // plain text body
+        html: `<b>Message from:</b> ${name} &lt;${email}&gt;<br><br>${message}`, // html body
+      });
+  
+      res.send('Message sent: ' + info.messageId);
+    } catch (error) {
+      res.status(500).send('Message not sent.');
+    }
+  });
