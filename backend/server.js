889bcfe55b657c18959729c3c7ae68d02c702dae
Michelle <mvflandin@gmail.com>
2023-11-07 19:48:07 -0800

    Add backend configuration for recaptcha

--- backend/server.js
+++ backend/server.js
@@ -1,56 +1,101 @@
 require('dotenv').config();
-
 const express = require('express');
+const cors = require('cors');
+const nodemailer = require('nodemailer');
+const fs = require('fs');
+const { RecaptchaEnterpriseServiceClient } = require('@google-cloud/recaptcha-enterprise');
+
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(express.json()); // Middleware to parse JSON bodies
+let credentials;
+let project_id;
+
+try {
+  credentials = JSON.parse(fs.readFileSync(process.env.GOOGLE_APPLICATION_CREDENTIALS));
+  project_id = credentials.project_id;
+} catch (error) {
+  console.error('Error reading or parsing credentials file:', error);
+  process.exit(1); 
+}
+
+const recaptchaClient = new RecaptchaEnterpriseServiceClient();
+
+app.use(express.json());
+app.use(cors({ origin: 'http://localhost:3000' }));
 
 // Test route to make sure your server is working
 app.get('/api', (req, res) => {
   res.send('Backend API is working!');
 });
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+// Nodemailer transporter setup
+const transporter = nodemailer.createTransport({
+  host: process.env.EMAIL_HOST,
+  port: process.env.EMAIL_PORT,
+  secure: false, // true for 465, false for other ports
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
 });
 
-// ADD CORS to communicate with frontend
-const cors = require('cors');
-app.use(cors({ origin: 'http://localhost:3000' }));
-
-
-// NODEMAILER SETUP
-
-const nodemailer = require('nodemailer');
-
-// Create a transporter object using the default SMTP transport
-let transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    secure: false,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS
-    }
-  });
 
 // Email sending route
 app.post('/api/sendmail', async (req, res) => {
-    let { name, email, message } = req.body;
-  
+  const { name, email, message, recaptchaToken } = req.body;
+
+  // Function to create an assessment for reCAPTCHA Enterprise
+  async function createAssessment(token) {
+    const projectPath = recaptchaClient.projectPath(project_id);
+    const assessment = {
+      event: {
+        token: token,
+        siteKey: process.env.SITE_KEY,
+      },
+    };
+
+    const request = {
+      assessment,
+      parent: projectPath,
+    };
+
     try {
-      // send mail with defined transport object
-      let info = await transporter.sendMail({
+      const [response] = await recaptchaClient.createAssessment(request);
+      return response;
+    } catch (error) {
+      console.error('Error with reCAPTCHA assessment:', error);
+      throw error; 
+    }
+  }
+
+  try {
+    const assessmentResponse = await createAssessment(recaptchaToken);
+
+    if (
+      assessmentResponse &&
+      assessmentResponse.tokenProperties.valid &&
+      assessmentResponse.riskAnalysis.score >= 0.5
+    ) {
+      const info = await transporter.sendMail({
         from: `"My Website Contact Form" <${process.env.EMAIL_USER}>`,
         to: process.env.EMAIL_TO,
-        subject: "New email from michellef.dev", 
-        text: `Message from: ${name} <${email}>\n\n${message}`, 
+        subject: "New email from michellef.dev",
+        text: `Message from: ${name} <${email}>\n\n${message}`,
         html: `<b>Message from:</b> ${name} &lt;${email}&gt;<br><br>${message}`,
       });
-  
+
       res.send('Message sent: ' + info.messageId);
-    } catch (error) {
-      res.status(500).send('Message not sent.');
+    } else {
+      // reCAPTCHA validation failed
+      res.status(400).send('reCAPTCHA validation failed.');
     }
-  });
+  } catch (error) {
+    console.error('Error sending email:', error);
+    res.status(500).send('Message not sent.');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
