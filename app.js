5bf0b9d9447641189a8dbcd99e163515ff40785c
michellevit <mvflandin@gmail.com>
2024-03-07 12:58:48 -0800

    Update folder structure and deployment

--- app.js
+++ app.js
@@ -12,6 +12,13 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 
+// serve the React application's build directory as static files
+app.use(express.static(path.join(__dirname, 'reactapp/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'reactapp/build', 'index.html'));
+});
+
 const recaptchaClient = new RecaptchaEnterpriseServiceClient();
 
 app.use(express.json());
