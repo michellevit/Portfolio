793cd06da14aa1f06ed3156afa8b76e846d58821
michellevit <mvflandin@gmail.com>
2024-03-19 17:06:42 -0700

    Remove first paragraph

--- reactapp/src/Pages/About.js
+++ reactapp/src/Pages/About.js
@@ -31,22 +31,22 @@ function About() {
       color: "#f6c7e0",
     });
     const a7 = annotate(document.querySelector("#e7"), {
-      type: "highlight",
+      type: "circle",
       color: "#f0dd81",
     });
-    const a8 = annotate(document.querySelector("#e8"), {
-      type: "highlight",
-      color: "#fdc3bf",
-    });
-    const a9 = annotate(document.querySelector("#e9"), {
-      type: "highlight",
-      color: "#e7cffe",
-    });
-    const e10 = annotate(document.querySelector("#e10"), {
-      type: "circle",
-      color: "#f6c7e0",
-    });
-    const ag = annotationGroup([a1, a2, a3, a4, a5, a6, a7, a8, a9, e10]);
+    // const a8 = annotate(document.querySelector("#e8"), {
+    //   type: "highlight",
+    //   color: "#fdc3bf",
+    // });
+    // const a9 = annotate(document.querySelector("#e9"), {
+    //   type: "highlight",
+    //   color: "#e7cffe",
+    // });
+    // const e10 = annotate(document.querySelector("#e10"), {
+    //   type: "circle",
+    //   color: "#f6c7e0",
+    // });
+    const ag = annotationGroup([a1, a2, a3, a4, a5, a6, a7]);
     const screenWidth = window.innerWidth;
     if (screenWidth >= 0) {
       ag.show();
@@ -61,30 +61,25 @@ function About() {
           Vancouver.
         </h2>
         <p>
-          I love building applications that {" "}
-          <span id="e2">solve real-world problems</span> and{" "}
-          <span id="e3">save people time</span>.
-        </p>
-        <p>
           I first encountered computer science at{" "}
-          <span id="e4">UBC</span>, and though my path led me into{" "}
-          <span id="e5">administration and accounting</span>,{" "}
+          <span id="e1">UBC</span>, and though my path led me into{" "}
+          <span id="e2">administration and accounting</span>,{" "}
           I found myself gravitating back to towards coding as I discovered the value in{" "}
-          <Link to="/amazon-checker"><span id="e6">automating simple tasks</span></Link>{" "}
+          <Link to="/amazon-checker"><span id="e3">automating simple tasks</span></Link>{" "}
           and{" "}
-          <Link to="/production-planner"><span id="e7">modernizing workflows</span></Link>.
+          <Link to="/production-planner"><span id="e4">modernizing workflows</span></Link>.
         </p>
         <p>
           I'm currently working on integrating courier APIs into my{" "}
-          <Link to="/production-planner"><span id="e8">Production Planner</span></Link>{" "}
+          <Link to="/production-planner"><span id="e5">Production Planner</span></Link>{" "}
           app, and creating a{" "}
-          <Link to="/fantasy-dg"><span id="e9">Fantasy Disc Golf</span></Link>{" "}
+          <Link to="/fantasy-dg"><span id="e6">Fantasy Disc Golf</span></Link>{" "}
           app as a
           fun side-project. 
         </p>
         <p>
           Please don't hesitate to{" "}
-          <span id="e10" className="about-contact">
+          <span id="e7" className="about-contact">
             <li>
               <Link to="/contact">contact me</Link>
             </li>
