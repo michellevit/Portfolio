bebc0d794c19043593725662c6848aad06cbf5e4
michellevit <mvflandin@gmail.com>
2024-09-06 13:46:59 -0700

    Update Amazon Checker project page

--- reactapp/src/Projects/AmazonChecker.js
+++ reactapp/src/Projects/AmazonChecker.js
@@ -4,6 +4,7 @@ import YouTubeEmbed from "./YouTubeEmbed";
 import projectDescriptions from "./ProjectDescriptions.json";
 import projectPic from "../static/amazon-checker-mockup.jpg";
 import pythonLogo from "../static/logos/python-logo.png";
+import pyinstallerLogo from "../static/logos/pyinstaller-logo.png";
 import openpyxlLogo from "../static/logos/openpyxl-logo.png";
 import githubLogo from "../static/logos/github-logo.png";
 
@@ -15,7 +16,7 @@ function AmazonChecker() {
         {/* <div><img src={projectPic} alt="Amazon inventory checker project snapshot"/></div> */}
         <div>{projectDescriptions.amazon_checker.long}</div>
         <a
-          href="https://github.com/michellevit/Amazon-Inventory-Checker"
+          href="https://github.com/michellevit/Amazon-Inventory-Checker-App"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -26,25 +27,28 @@ function AmazonChecker() {
       </div>
       <div>
         <h3>Demo</h3>
-        <YouTubeEmbed embedId="lD-wTry930w" />
+        <YouTubeEmbed embedId="JX_15M3vQFNk546g" />
       </div>
       <div className="key-features">
         <h3>Key Features</h3>
         <ul>
-          <li>Custom Greedy Algorithm</li>
-          <li>Spreadsheet Reading/Writing</li>
-          <li>Easy Batch Script Deployment</li>
+          <li>Graphical User Interface</li>
+          <li>Interactive Spreadsheet Handling</li>
+          <li>Easy User Experience</li>
         </ul>
       </div>
       <div className="technologies-used">
         <h3>Technologies Used</h3>
         <ul className="tech">
           <li>Python</li>
+          <li>Tkinter</li>
+          <li>PyInstaller</li>
           <li>OpenPyXL</li>
           <li>GitHub</li>
         </ul>
         <div className="logo-images">
           <img src={pythonLogo} alt="Python logo" />
+          <img src={pyinstallerLogo} alt="PyInstaller logo" />
           <img id="openpy" src={openpyxlLogo} alt="OpenPyXL logo" />
           <img src={githubLogo} alt="GitHub logo" />
         </div>
