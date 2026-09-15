961d2bd1ee1ab9a653c61da2f601a7d0b2abff5c
michellevit <mvflandin@gmail.com>
2026-09-15 13:50:55 -0700

    Fix success message typo

--- reactapp/src/Pages/Contact.js
+++ reactapp/src/Pages/Contact.js
@@ -1,15 +1,15 @@
-import React, { useState, useRef } from 'react';
-import { useForm, ValidationError } from '@formspree/react';
-import ReCAPTCHA from 'react-google-recaptcha';
-import './Contact.css';
+import React, { useState, useRef } from "react";
+import { useForm, ValidationError } from "@formspree/react";
+import ReCAPTCHA from "react-google-recaptcha";
+import "./Contact.css";
 
 function Contact() {
   const [state, handleSubmit] = useForm("mpwpabkn");
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [errorMessage, setErrorMessage] = useState(null);
-  const [successMessage, setSuccessMessage] = useState('');
-  const [successClass, setSuccessClass] = useState('');
-  
+  const [successMessage, setSuccessMessage] = useState("");
+  const [successClass, setSuccessClass] = useState("");
+
   const recaptchaRef = useRef();
   const formRef = useRef(null);
 
@@ -17,24 +17,23 @@ function Contact() {
     event.preventDefault();
     setIsSubmitting(true);
     setErrorMessage(null);
-    setSuccessMessage('');
+    setSuccessMessage("");
 
     try {
-  
       if (!recaptchaRef.current) {
         setErrorMessage("reCAPTCHA validation is not available.");
         setIsSubmitting(false);
         return;
       }
-  
+
       const token = await recaptchaRef.current.executeAsync();
-  
+
       if (!token) {
         setErrorMessage("Please complete the reCAPTCHA.");
         setIsSubmitting(false);
         return;
       }
-  
+
       recaptchaRef.current.reset(); // Reset reCAPTCHA for future submissions
 
       // Convert FormData to JSON object
@@ -43,12 +42,14 @@ function Contact() {
       jsonData["g-recaptcha-response"] = token;
 
       const result = await handleSubmit(jsonData);
-  
+
       if (result && result.errors) {
         setErrorMessage("There was an issue with the form submission.");
       } else {
         // Success handling
-        setSuccessMessage("Thank you for message, I'll get back to you soon!");
+        setSuccessMessage(
+          "Thank you for your message, I'll get back to you soon!",
+        );
         setSuccessClass("success-message");
 
         // Reset the form
@@ -57,7 +58,9 @@ function Contact() {
         }
       }
     } catch (error) {
-      setErrorMessage("Failed to send the message. Please reach me at contact@michellef.dev");
+      setErrorMessage(
+        "Failed to send the message. Please reach me at contact@michellef.dev",
+      );
     } finally {
       setIsSubmitting(false);
     }
@@ -68,7 +71,12 @@ function Contact() {
       <h2>Contact Me</h2>
       {errorMessage && <p className="error-message">{errorMessage}</p>}
 
-      <form ref={formRef} onSubmit={handleFormSubmit} action="https://formspree.io/f/mpwpabkn" method="POST">
+      <form
+        ref={formRef}
+        onSubmit={handleFormSubmit}
+        action="https://formspree.io/f/mpwpabkn"
+        method="POST"
+      >
         <label htmlFor="name">Your Name:</label>
         <input id="name" type="text" name="name" required />
         <ValidationError prefix="Name" field="name" errors={state.errors} />
@@ -79,7 +87,11 @@ function Contact() {
 
         <label htmlFor="message">Your Message:</label>
         <textarea id="message" name="message" required />
-        <ValidationError prefix="Message" field="message" errors={state.errors} />
+        <ValidationError
+          prefix="Message"
+          field="message"
+          errors={state.errors}
+        />
 
         {/* Google reCAPTCHA v2 Invisible Widget */}
         <ReCAPTCHA
@@ -89,8 +101,13 @@ function Contact() {
           badge="bottomright"
         />
 
-        <button type="submit" id="contact-form" disabled={isSubmitting} className="dark">
-          {isSubmitting ? 'Sending...' : 'Send'}
+        <button
+          type="submit"
+          id="contact-form"
+          disabled={isSubmitting}
+          className="dark"
+        >
+          {isSubmitting ? "Sending..." : "Send"}
         </button>
       </form>
 
